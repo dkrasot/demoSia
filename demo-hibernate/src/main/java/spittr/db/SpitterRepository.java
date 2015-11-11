4925cfacce31e80f032c36bf5d89afed7d0865c3
dkrasot <ik5209101>
2015-11-12 00:10:06 +0200

    (0) add hibernate 4 demo

---
+++ demo-hibernate/src/main/java/spittr/db/SpitterRepository.java
@@ -0,0 +1,19 @@
+package spittr.db;
+
+import spittr.domain.Spitter;
+
+import java.util.List;
+
+public interface SpitterRepository {
+
+    long count();
+
+    Spitter save(Spitter spitter);
+
+    Spitter findOne(long id);
+
+    Spitter findByUsername(String username);
+
+    List<Spitter> findAll();
+
+}
